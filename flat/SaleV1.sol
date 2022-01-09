b51d182ed2e64983741938ae208e7fc32b9de4e5
elixirevo <elixirevocation@gmail.com>
2022-01-09 23:47:25 +0900

    flat: sale v1

---
+++ flat/SaleV1.sol
@@ -0,0 +1,385 @@
+// Sources flattened with hardhat v2.8.1 https://hardhat.org
+
+// File contracts/openzeppelin/contracts/utils/Context.sol
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
+
+
+// File contracts/openzeppelin/contracts/utils/math/SafeMath.sol
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+// CAUTION
+// This version of SafeMath should only be used with Solidity 0.8 or later,
+// because it relies on the compiler's built in overflow checks.
+
+/**
+ * @dev Wrappers over Solidity's arithmetic operations.
+ *
+ * NOTE: `SafeMath` is no longer needed starting with Solidity 0.8. The compiler
+ * now has built in overflow checking.
+ */
+library SafeMath {
+    /**
+     * @dev Returns the addition of two unsigned integers, with an overflow flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryAdd(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            uint256 c = a + b;
+            if (c < a) return (false, 0);
+            return (true, c);
+        }
+    }
+
+    /**
+     * @dev Returns the substraction of two unsigned integers, with an overflow flag.
+     *
+     * _Available since v3.4._
+     */
+    function trySub(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            if (b > a) return (false, 0);
+            return (true, a - b);
+        }
+    }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, with an overflow flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryMul(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+            // benefit is lost if 'b' is also tested.
+            // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
+            if (a == 0) return (true, 0);
+            uint256 c = a * b;
+            if (c / a != b) return (false, 0);
+            return (true, c);
+        }
+    }
+
+    /**
+     * @dev Returns the division of two unsigned integers, with a division by zero flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryDiv(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            if (b == 0) return (false, 0);
+            return (true, a / b);
+        }
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers, with a division by zero flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryMod(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            if (b == 0) return (false, 0);
+            return (true, a % b);
+        }
+    }
+
+    /**
+     * @dev Returns the addition of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `+` operator.
+     *
+     * Requirements:
+     *
+     * - Addition cannot overflow.
+     */
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a + b;
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting on
+     * overflow (when the result is negative).
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     *
+     * - Subtraction cannot overflow.
+     */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a - b;
+    }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `*` operator.
+     *
+     * Requirements:
+     *
+     * - Multiplication cannot overflow.
+     */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a * b;
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers, reverting on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator.
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a / b;
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * reverting when dividing by zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a % b;
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting with custom message on
+     * overflow (when the result is negative).
+     *
+     * CAUTION: This function is deprecated because it requires allocating memory for the error
+     * message unnecessarily. For custom revert reasons use {trySub}.
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     *
+     * - Subtraction cannot overflow.
+     */
+    function sub(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        unchecked {
+            require(b <= a, errorMessage);
+            return a - b;
+        }
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers, reverting with custom message on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function div(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        unchecked {
+            require(b > 0, errorMessage);
+            return a / b;
+        }
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * reverting with custom message when dividing by zero.
+     *
+     * CAUTION: This function is deprecated because it requires allocating memory for the error
+     * message unnecessarily. For custom revert reasons use {tryMod}.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function mod(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        unchecked {
+            require(b > 0, errorMessage);
+            return a % b;
+        }
+    }
+}
+
+
+// File contracts/interfaces/IPaperChildrenV1.sol
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.10;
+
+interface IPaperChildrenV1 {
+	event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+
+	event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
+
+	event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+
+	function mint(address to) external;
+
+	function totalSupply() external view returns (uint256);
+
+	function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256 tokenId);
+
+	function tokenByIndex(uint256 index) external view returns (uint256);
+
+	function balanceOf(address owner) external view returns (uint256 balance);
+
+	function ownerOf(uint256 tokenId) external view returns (address owner);
+
+	function transferFrom(
+		address from,
+		address to,
+		uint256 tokenId
+	) external;
+
+	function approve(address to, uint256 tokenId) external;
+
+	function getApproved(uint256 tokenId) external view returns (address operator);
+
+	function setApprovalForAll(address operator, bool _approved) external;
+
+	function isApprovedForAll(address owner, address operator) external view returns (bool);
+
+	function massTransferFrom(
+		address from,
+		address to,
+		uint256[] memory _myTokensId
+	) external;
+}
+
+
+// File contracts/Sale/SaleV1.sol
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.10;
+
+
+
+contract SaleV1 is Context {
+	using SafeMath for uint256;
+
+	IPaperChildrenV1 public PaperChildrenContract;
+	uint256 PRICE = 50000000000000000000; //50 klay
+	uint256 MAX_NFT_SUPPLY = 1003;
+	bool public isSale = false;
+	address public devAddress;
+	address public C1;
+	address public C2;
+
+	mapping(address => bool) public buyerList;
+
+	modifier onlyCreator() {
+		require(devAddress == _msgSender(), "onlyCreator: caller is not the creator");
+		_;
+	}
+
+	modifier saleRole() {
+		require(isSale, "The free sale has not started.");
+		require(PaperChildrenContract.totalSupply() < MAX_NFT_SUPPLY, "Sale has already ended.");
+		require(buyerList[_msgSender()] == false, "Public sale can participate only once.");
+		require(PRICE <= msg.value, "KLAY value sent is not correct");
+		_;
+	}
+
+	constructor(
+		address v1Contract,
+		address _C1,
+		address _C2,
+		address _dev
+	) {
+		PaperChildrenContract = IPaperChildrenV1(v1Contract);
+		C1 = _C1;
+		C2 = _C2;
+		devAddress = _dev;
+	}
+
+	function publicSale() public payable saleRole {
+		PaperChildrenContract.mint(_msgSender());
+		buyerList[_msgSender()] = true;
+	}
+
+	function preMint(address _to) public payable onlyCreator {
+		PaperChildrenContract.mint(_to);
+	}
+
+	function withdraw() public payable onlyCreator {
+		uint256 contractBalance = address(this).balance;
+		uint256 percentage = contractBalance / 100;
+
+		require(payable(C1).send(percentage * 34));
+		require(payable(C2).send(percentage * 66));
+	}
+
+	function setDevAddress(address changeAddress) public onlyCreator {
+		devAddress = changeAddress;
+	}
+
+	function setC1(address changeAddress) public onlyCreator {
+		C1 = changeAddress;
+	}
+
+	function setC2(address changeAddress) public onlyCreator {
+		C2 = changeAddress;
+	}
+
+	function setSale() public onlyCreator {
+		isSale = !isSale;
+	}
+}
